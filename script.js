7995eba72024bfee766508d9daabe7cf416b6f21
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-09 18:42:07 +0530

    Handling Click Events added

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// Selecting and Manipulating Elements
+/* console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.number').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
+document.querySelector('.guess').value = 23; */
+
+
+
+// Handling Click Events
+document.querySelector(".check").addEventListener("click", function() {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(typeof guess);
+
+    if(!guess) {           // guess is equal to 0 at this point and 0 as a boolean always return false and we want this false  
+        document.querySelector(".message").textContent =      // to be true so that's why we used !guess (!false, i.e, true).
+        "⛔No Number!" ;                     
+    }
+});
